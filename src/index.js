a7ed3366c250af7aa676b2ea0434c410169bdb98
tereferrara <104356538+tereferrara@users.noreply.github.com>
2022-04-29 19:43:20 +0100

    axios installed and temperature function created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("hello");
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector(`#temperature`);
+    let cityElement = document.querySelector(`#city`);
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = (response.data.name);
+}
+
+let apiKey = "09d4e7600a7b696d5e69d0366d8b8483";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+axios.get(apiUrl).then(displayTemperature);
